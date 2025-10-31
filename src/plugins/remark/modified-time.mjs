3eba6685769d88cb40b747e3cb93c160834a6073
grassblock <i@gb0.dev>
2025-10-31 22:07:25 +0800

    fix: properly catch the git command error

--- src/plugins/remark/modified-time.mjs
+++ src/plugins/remark/modified-time.mjs
@@ -5,9 +5,9 @@ export function remarkModifiedTime() {
     return function (tree, file) {
         const filepath = file.history[0];
         try {
-            file.data.astro.frontmatter.lastModified = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`).toString();
-        } catch {
-            console.log("fetch time from git log failed, falling back to date");
+            file.data.astro.frontmatter.lastModified = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`,{ encoding: 'utf8' }).toString();
+        } catch(error) {
+            console.log("fetch time from git log failed, falling back to file modification date");
             file.data.astro.frontmatter.lastModified = statSync(filepath).mtime.toISOString();
         }
     };
