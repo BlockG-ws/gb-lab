d09aaafd492f868f2043ecbc86526de5ea27db1d
grassblock <i@gb0.dev>
2025-10-31 21:47:00 +0800

    fix: build fails when the site not in a git repo

--- src/plugins/remark/modified-time.mjs
+++ src/plugins/remark/modified-time.mjs
@@ -4,6 +4,11 @@ import {statSync} from "node:fs";
 export function remarkModifiedTime() {
     return function (tree, file) {
         const filepath = file.history[0];
-        file.data.astro.frontmatter.lastModified = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`).toString() || statSync(filepath).mtime.toISOString();
+        try {
+            file.data.astro.frontmatter.lastModified = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`).toString();
+        } catch {
+            console.log("fetch time from git log failed, falling back to date");
+            file.data.astro.frontmatter.lastModified = statSync(filepath).mtime.toISOString();
+        }
     };
 }
